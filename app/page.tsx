3748e999a22d502f301790fd074a2e4e81702971
Austin Winslow <austin@austinwinslow.com>
2025-05-03 16:51:15 -0700

    Created homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,21 @@
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+
 export default function Home() {
     return (
-	<h1>Hello from React!!</h1>
+	<>
+	    <Header />
+		<main className="content">
+		    <h1 className="title">The Oddish Arcanum</h1>
+		    <h2 className="subtitle">my personal grimoire</h2>
+		    <img className="gif" src="/images/dancing-bmo.gif" alt="Dancing BMO" />
+		    <br />
+		    <p className="text text-center">
+			Feel free to look around! The site is a work in progress though ... so be warned!
+		    </p>		    
+		    <br />
+		</main>
+	    <Footer />
+	</>
     )
 }
