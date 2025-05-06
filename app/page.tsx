f7f56f7bcc71a361f2e21dd30c87f709210af140
Austin Winslow <austin@austinwinslow.com>
2025-05-06 14:39:17 -0700

    Finished base homepage, added some styling

--- app/page.tsx
+++ app/page.tsx
@@ -6,13 +6,12 @@ export default function Home() {
 	<>
 	    <Header />
 		<main className="content">
-		    <h1 className="title">The Oddish Arcanum</h1>
-		    <h2 className="subtitle">my personal grimoire</h2>
+		    <h1 className="title">Welcome, Cyber-Traveller</h1>
 		    <img className="my-avatar" src="/images/oddish1-avatar.png" alt="Oddish1 Avatar" />
-		    <br />
-		    <p className="text text-center">
-			Feel free to look around! The site is a work in progress though ... so be warned!
-		    </p>		    
+		    <h2 className="subtitle">here you can find everything i have to share</h2>
+			<p className="text blurb">
+				From images sealed within my gallery, musings scrawled out on my blog, or a look into the machinations of my mind... I'm sure there is something here to interest you. Feel free to look around! This place is teeming with code I have yet to subdue. Be warned...
+			</p>	    
 		    <br />
 		</main>
 	    <Footer />
